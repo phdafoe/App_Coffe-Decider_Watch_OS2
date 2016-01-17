9469ac2be4caa89431d15acc7840fb0cb0c67dfe
phdafoe <phdafoe@gmail.com>
2016-01-17 16:52:35 +0100

    Initial Commit

---
+++ App_Coffe Decider_Watch_OS2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  App_Coffe Decider_Watch_OS2 WatchKit Extension
+//
+//  Created by User on 17/1/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
