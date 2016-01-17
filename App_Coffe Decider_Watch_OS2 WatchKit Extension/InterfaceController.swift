7653a1c92b5888d96be9155f7824d4d0f26491fa
phdafoe <phdafoe@gmail.com>
2016-01-17 17:30:25 +0100

    actualizacion_170116_1730

--- App_Coffe Decider_Watch_OS2 WatchKit Extension/InterfaceController.swift
+++ App_Coffe Decider_Watch_OS2 WatchKit Extension/InterfaceController.swift
@@ -11,9 +11,84 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    //MARK: - VARIABLES LOCALES GLOBALES
+    
+    var myCoffeeArray = ["Coffee", "Ice Coffee", "Americano", "Espresso", "Ice Americano", "Latte", "Cappuccino", "Espresso Machiato", "Ice Latte", "Coffee Milkshake"]
+    
+    var myButtonArray = ["Choose Coffee", "Another", "Try again"]
+    
+    var myCoffeeArrayMAX : Int = 0
+    var myButtonArrayMAX : Int = 0
+    
+    var myCoffeeArrayRandom : Int = 0
+    var myButtonArrayRandom : Int = 0
+    
+    
 
+    
+    //MARK: - IBOUTLET
+    
+    
+    @IBOutlet var myLblCoffee: WKInterfaceLabel!
+    @IBOutlet var myBtnChooseCoffee: WKInterfaceButton!
+    
+    
+    
+    //MARK: - IBACTION
+    
+
+    @IBAction func btnChooseCoffeeACTION() {
+        
+        randomizeTheNumbers()
+        
+        
+    }
+    
+    
+    //MARK: - UTILS
+    
+    func findMaxItemsInArray(){
+        
+        myCoffeeArrayMAX = myCoffeeArray.count
+        myButtonArrayMAX = myButtonArray.count
+        
+    }
+    
+    
+    
+    func randomizeTheNumbers(){
+        
+        myCoffeeArrayRandom = Int(arc4random_uniform(UInt32(myCoffeeArrayMAX)))
+        myButtonArrayRandom = Int(arc4random_uniform(UInt32(myButtonArrayMAX)))
+        
+        printCoffee()
+        printButton()
+        
+    }
+    
+    func printCoffee(){
+        
+        myLblCoffee.setText("\(myCoffeeArray[myCoffeeArrayRandom])")
+        
+    }
+    
+    func printButton(){
+        
+        myBtnChooseCoffee.setTitle("\(myButtonArray[myButtonArrayRandom])")
+        
+        
+    }
+    
+    
+    
+    
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
+        
+        
+        findMaxItemsInArray()
         
         // Configure interface objects here.
     }
